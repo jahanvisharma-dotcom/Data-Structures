b19479cd1bf81d45cde3c33d751ba19192aad9dc
Jahanvi Sharma <56484763+jahanvisharma-dotcom@users.noreply.github.com>
2021-01-11 23:00:25 +0530

    Create Matrix Chain Multiplication using DP.py

---
+++ Matrix Chain Multiplication using DP.py
@@ -0,0 +1,35 @@
+ 
+import sys 
+
+# Matrix A[i] has dimension p[i-1] x p[i] 
+# for i = 1..n 
+
+
+def MatrixChainOrder(p, i, j): 
+
+	if i == j: 
+		return 0
+
+	_min = sys.maxsize 
+
+	for k in range(i, j): 
+
+		count = (MatrixChainOrder(p, i, k) 
+				+ MatrixChainOrder(p, k + 1, j) 
+				+ p[i-1] * p[k] * p[j]) 
+
+		if count < _min: 
+			_min = count 
+
+
+	return _min 
+
+
+
+arr = [1, 2, 3, 4, 3] 
+n = len(arr) 
+
+print("Minimum number of multiplications is ", 
+	MatrixChainOrder(arr, 1, n-1)) 
+
+
